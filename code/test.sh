a9ae6abf109d6152599cc25ed1441d8a897cea69
Daniel Ng See Cheong <danielpnsc@gmail.com>
2016-09-26 15:26:47 +0200

    Testing no solution

--- code/test.sh
+++ code/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 output=$(./sudoku < test)
 solution=$(cat ./solution)
-echo Easy
+echo -n Easy:
 if [ "$output" == "$solution" ]
 then
 	echo Correct;
@@ -12,7 +12,18 @@ fi
 
 output=$(./sudoku < test2)
 solution=$(cat ./solution2)
-echo Difficult
+echo -n Difficult:
+if [ "$output" == "$solution" ]
+then
+        echo Correct;
+else
+        echo Incorrect;
+        exit 1; #Error status
+fi
+
+output=$(./sudoku < test3)
+solution=$(cat ./solution3)
+echo -n No solution:
 if [ "$output" == "$solution" ]
 then
         echo Correct;

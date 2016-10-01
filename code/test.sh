45dcee4e2b2db6c2782a2916455145d3088af1df
Daniel Ng See Cheong <danielpnsc@gmail.com>
2016-10-01 20:01:36 +0200

    database like testing created Added version_0 (original version) to README

--- code/test.sh
+++ code/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-output=$(./sudoku < test)
-solution=$(cat ./solution)
+
+DATALOC="database"
+
+output=$(./sudoku < $DATALOC/test)
+solution=$(cat $DATALOC/solution)
 echo -n Easy:
 if [ "$output" == "$solution" ]
 then
@@ -10,8 +13,8 @@ else
 	exit 1; #Error status
 fi
 
-output=$(./sudoku < test2)
-solution=$(cat ./solution2)
+output=$(./sudoku < $DATALOC/test2)
+solution=$(cat $DATALOC/solution2)
 echo -n Difficult:
 if [ "$output" == "$solution" ]
 then
@@ -21,8 +24,8 @@ else
         exit 1; #Error status
 fi
 
-output=$(./sudoku < test3)
-solution=$(cat ./solution3)
+output=$(./sudoku < $DATALOC/test3)
+solution=$(cat $DATALOC/solution3)
 echo -n No solution:
 if [ "$output" == "$solution" ]
 then
